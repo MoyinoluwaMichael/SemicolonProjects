4b67e1b4c96692e1eadc7dbb02672032a023c5bc
Moyinoluwa Michael <ogunsmoyin.m@gmail.com>
2023-01-11 19:42:50 +0100

    Updated

--- src/christmassSnacks/myersBriggsQuestionnaire/MyersBriggsQuestionnaireMain.java
+++ src/christmassSnacks/myersBriggsQuestionnaire/MyersBriggsQuestionnaireMain.java
@@ -7,7 +7,6 @@ public class MyersBriggsQuestionnaireMain {
         newPersonality.setCandidateName();
         MyersBriggsQuestionnaire.displayQuestions();
         MyersBriggsQuestionnaire.analyzeResponses();
-
         newPersonality.printPersonalityTrait();
     }
 }
